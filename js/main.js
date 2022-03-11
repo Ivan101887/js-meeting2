72718b65ff7dbe2566c6a4a67045a2684b6607ef
Ivan yuan <Ivan_Yuan@cmoney.com.tw>
2022-03-11 16:28:10 +0800

    調整loader位置、js變數改成區域變數

--- js/main.js
+++ js/main.js
@@ -2,11 +2,10 @@ let data = [];
 const elemCardBox = document.querySelector('#CardBox');
 const elemTown = document.querySelector('#Town');
 const elemCity = document.querySelector('#City');
-const elemLoading = document.querySelector('#Loading');
-let processedData = []
 setInit();
 setEvent();
 async function setInit() {
+  const elemLoading = document.querySelector('#Loading');
   await getData();
   elemLoading.classList.add('js-loader');
   render();
